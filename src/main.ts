7c1d1dad1009faaccce94566b71a838a682fa869
tangopj <rail.batyrshin.js@gmail.com>
2025-08-01 21:47:03 +0600

    added a log

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Mustache from "mustache";
 import { metadata } from "./lib/metadata";
 
 const render = () => {
+  console.log("Running...");
   const template = document.querySelector("#app")!;
   const htmlContent = template.innerHTML;
   const rendered = Mustache.render(htmlContent, metadata);
